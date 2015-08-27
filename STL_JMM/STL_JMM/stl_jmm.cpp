b669385fcf05605d7a5820f94dd1b16707d47ef1
StupidHod <jememe@163.com>
2015-08-27 13:22:12 +0800

    git init

---
+++ STL_JMM/STL_JMM/stl_jmm.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <utility>
+#include "jmm_allocator.h"
+#include <vector>
+#include "jmm_stl_iterator.h"
+
+
+using namespace std;
+
+struct B{};
+
+struct D1 :public B{};
+
+struct D2 :public D1{};
+
+template <class I>
+void func(I& p, B)
+{
+
+	cout << "B version" << endl;
+
+}
+
+template <class I>
+void func(I& p, D2)
+{
+
+	cout << "D2 version" << endl;
+
+}
+
+
+int main()
+{
+	int *p;
+
+	func(p, B());
+	func(p, D1());
+
+
+	func(p, D2());
+}
