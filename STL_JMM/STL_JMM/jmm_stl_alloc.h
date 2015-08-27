40b6f00610b1048466c6aebe8f2aac1695d88514
StupidHod <jememe@163.com>
2015-08-28 00:48:36 +0800

    update at dorm, for alloc and construct

--- STL_JMM/STL_JMM/jmm_stl_alloc.h
+++ STL_JMM/STL_JMM/jmm_stl_alloc.h
@@ -15,7 +15,7 @@ namespace jmm_stl
 	private:
 		static void *oom_malloc(size_t);
 		static void *oom_realloc(void*, size_t);
-		static void(*__malloc_alloc_oom_handle)();
+		static void(*__malloc_alloc_oom_handler)();
 
 	public:
 		static void *allocate(size_t n)
@@ -51,8 +51,88 @@ namespace jmm_stl
 		
 		}
 
+
+		static void(*set_malloc_handler(void(*f)()))()
+		{
 		
+			void(*old)() = __malloc_alloc_oom_handler;
+			__malloc_alloc_oom_handler = f;
+			return (old);
+		
+		}
 
 	};
+
+
+	template <int inst>
+	void(*__malloc_alloc_template<inst>::__malloc_alloc_oom_handler)() = 0;
+
+	template<int inst>
+	void * __malloc_alloc_template<inst>::oom_malloc(size_t n)
+	{
 	
+		void(*my_malloc_handler)();
+		void *result;
+
+		for (;;)
+		{
+			my_malloc_handler = __malloc_alloc_oom_handler;
+
+			if (0 == my_malloc_handler)
+			{
+				__THROW_BAD_ALLOC;
+			}
+
+			(*my_malloc_handler)();
+			result = malloc(n);
+			if (result)
+			{
+				return result;
+			}
+		}
+	
+	}
+
+
+	template <int inst>
+	void * __malloc_alloc_template<inst>::oom_realloc(void*p, size_t n)
+	{
+		void(*my_malloc_handler)();
+		void *result;
+
+		for (;;)
+		{
+			my_malloc_handler = __malloc_alloc_oom_handler;
+
+			if (0 == my_malloc_handler)
+			{
+				__THROW_BAD_ALLOC;
+			}
+
+			(*my_malloc_handler)();
+			result = realloc(p,n);
+			if (result)
+			{
+				return result;
+			}
+		}
+	
+	}
+
+	typedef __malloc_alloc_template<0> malloc_alloc;
+
+
+	union obj
+	{
+		union obj* free_list_link;
+		char client_data[1];
+
+
+	};
+
+
+	enum{__ALIGN = 8};
+	enum{ __MAX_BYTES = 128 };
+	enum{__NFREELISTS = __MAX_BYTES/__ALIGN};
+
 }
